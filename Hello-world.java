00cc83e9551154c1de83e096f9676ff5e7d4965d
divyaADMIN <137484788+divya6688@users.noreply.github.com>
2023-09-12 17:27:38 +0530

    Update Hello-world.java

--- Hello-world.java
+++ Hello-world.java
@@ -2,6 +2,6 @@
 
 class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, World!and divya welcome to java world and devops world"); 
+        System.out.println("Hello, World!and divya welcome to java world and devops world and git commands"); 
     }
 }
